5239b2c16130173b9fabf54a006d48ad4100a5c1
Vladislava Scherbo <vladislava.volokhovich@gmail.com>
2025-02-25 02:14:11 +0300

    Добавлен экран загрузки

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,11 +1,15 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
 }
+
 
 /*
  Mock-данные
